671b7ef4906b2b94ac943ca4d7c02ad2bf82b26a
Vadim Abdullaev <vadim.abdullaev@amdaris.com>
2016-07-01 18:23:01 +0300

    create proj

---
+++ DuckTest/Infrastructure/Implementation/NormalQuack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckTest.Infrastructure.Implementation
+{
+    public class NormalQuack : IQuackable
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Quack!");
+        }
+    }
+}
